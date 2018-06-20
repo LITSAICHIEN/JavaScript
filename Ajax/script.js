c1d44336a63ffeb2cf28e23dc9237fe2f7624be0
LITSAICHIEN <39357707+LITSAICHIEN@users.noreply.github.com>
2018-06-20 18:03:29 +0900

    Add files via upload

--- Ajax/script.js
+++ Ajax/script.js
@@ -5,7 +5,7 @@
     dataType: 'json',
     success: function(data){
           for(var i in data){
-          $('#output').append('<tr><th colspan="2">' + data[i].division + '</th></tr>'+'<tr><th>お名前</th><th>歳</th></tr>');
+          $('#output').append('<tr><th colspan="2" class="first">' + data[i].division + '</th></tr>'+'<tr><th scope="col">お名前</th><th scope="col">年齢</th></tr>');
               for(var j in data[i].person){
                   $('#output').append('<tr><td>' + data[i].person[j].name + '</td><td>' + data[i].person[j].age + '才</td></tr>');
               }
